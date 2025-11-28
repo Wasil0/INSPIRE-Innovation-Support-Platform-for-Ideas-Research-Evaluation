f456c2b20dcf1ad88687a6be34b6acd1ec6c4ca1
Wasil0 <20wasil04@gmail.com>
2025-11-28 22:17:12 +0500

    Added .env

--- Backend-z/db/db.py
+++ Backend-z/db/db.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
-MONGO_URI = os.getenv(
-    "MONGO_URI",
-    "mongodb+srv://ansarirubas75_db_user:forfydpuse45@cluster0.8bmfqll.mongodb.net/"
-)
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise ValueError("No MONGO_URI found in environment variables")
 
 client = MongoClient(MONGO_URI)
 db = client["project_portal"]
