c1dbc61a97f326d8f0f83717bcfe372d02926017
DELL <rubasansari3@gmail.com>
2026-05-04 17:50:55 +0500

    updated backend APIs

--- src/pages/faculty/advisor/AdvisorDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdvisorDashboard = () => {
-  return <div></div>;
-};
-
-export default AdvisorDashboard;
--- src/pages/faculty/committee/CommitteeDashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CommitteeDashboard = () => {
-  return <div></div>;
-};
-
-export default CommitteeDashboard;
